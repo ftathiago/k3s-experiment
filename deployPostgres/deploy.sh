a122c82ac3f289d09e9c03ffeff7e0fa2f9d77b6
Francisco Thiago de Almeida <ftathiago@gmail.com>
2023-11-11 13:49:17 -0300

    feat: add postgress

--- deployPostgres/deploy.sh
+++ deployPostgres/deploy.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-# Install Postgres
-kubectl apply -f local-path-storage.yml
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-kubectl apply -f role-local-path.yaml
-kubectl apply -f pv.yaml
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-helm install dev-pg bitnami/postgresql \
-    --set primary.persistence.existingClaim=pg-pvc,auth.postgresPassword=pgpass,volumePermissions.enabled=true \
-    --set auth.database=WebApiDB \
-    --namespace postgres-database --create-namespace
-kubectl rollout status statefulset/dev-pg-postgresql -n default --request-timeout 5m
-kubectl apply -f service.yaml
-
-# helm install dev-pg bitnami/postgresql \
-#     --set primary.persistence.existingClaim=pg-pvc,auth.postgresPassword=pgpass,volumePermissions.enabled=true \
-#     --set auth.database=WebApiDB
+helm install postgres-container bitnami/postgresql --set persistence.existingClaim=postgres-pvc --set volumePermissions.enabled=true --namespace postgres-server
--- deployPostgres/undeploy.sh
+++ deployPostgres/undeploy.sh
@@ -1,8 +1,1 @@
-kubectl delete namespace postgres-database
-helm uninstall dev-pg
-kubectl delete -f deploy.sh
-kubectl delete -f local-path-storage.yml
-kubectl delete -f pv.yaml
-kubectl delete -f role-local-path.yaml
-kubectl delete -f service.yaml
-kubectl delete namespace postgres-storage
+kubectl delete namespace postgres-server
