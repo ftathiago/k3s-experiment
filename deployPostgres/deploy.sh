b05fa2aeb345ffa030113defeabfaf5388331125
Francisco Thiago de Almeida <ftathiago@gmail.com>
2023-11-19 11:16:48 -0300

    refact(database): Modify install script to apply pvc and service first

--- deployPostgres/deploy.sh
+++ deployPostgres/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+kubectl apply -f 01-pvc.yaml
+kubectl apply -f 04-service.yaml
+
 helm install postgres-container bitnami/postgresql --set persistence.existingClaim=postgres-pvc --set volumePermissions.enabled=true --namespace postgres-server
