c5951b41e604c7fda44e3830bc5d640558701add
Francisco Thiago de Almeida <ftathiago@gmail.com>
2023-12-10 20:31:46 -0300

    fix(longhorn): Change replica to 1

--- longhorn/install.sh
+++ longhorn/install.sh
@@ -1,8 +1,5 @@
-helm repo add longhorn https://charts.longhorn.io
-helm repo update
-
-kubectl create namespace longhorn-system
-helm upgrade -i longhorn longhorn/longhorn --namespace longhorn-system
+kubectl apply -f longhorn.yaml
+kubectl wait --for=condition=Ready pods --all -n longhorn-system
 
 
 # Turn longhorn default class (execute at controller)
@@ -10,9 +7,8 @@ helm upgrade -i longhorn longhorn/longhorn --namespace longhorn-system
 
 # sudo sed -i -e "s/storageclass.kubernetes.io\/is-default-class: \"true\"/storageclass.kubernetes.io\/is-default-class: \"false\"/g" /var/lib/rancher/k3s/server/manifests/custom-local-storage.yaml
 
-kubectl apply -f 02-storageClass.yaml 
-kubectl apply -f 03-pvcSample.yaml  
 kubectl apply -f ingress.yaml
+kubectl wait --for=jsonpath='{.status.loadBalancer.ingress[0].ip}'=192.168.1.145 ingress/longhorn-system-ingress -n longhorn-system
 
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-kubectl patch storageclass longhorn-fast -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
